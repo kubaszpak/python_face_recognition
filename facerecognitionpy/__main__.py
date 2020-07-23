10deb07e38ae46a156ac4dd19a12ba0d034349ab
kubaszpak <jakub.szpak.it@gmail.com>
2020-07-23 17:28:17 +0200

    Camera works now

--- facerecognitionpy/__main__.py
+++ facerecognitionpy/__main__.py
@@ -1,12 +1,20 @@
 import cv2
 
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+cap.set(10,100)
+
 def main():
-    
-    cap = cv2.videoCapture(0)
     while True:
         success, img = cap.read()
-        
-
+        cv2.imshow("Video", img)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+    cap.release()
+    cv2.destroyAllWindows() 
 
 if __name__ == "__main__":
     main()
