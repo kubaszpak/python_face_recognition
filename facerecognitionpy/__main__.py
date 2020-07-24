2fa0556c1909fcbea00fcefe74a2780f036508b3
kubaszpak <jakub.szpak.it@gmail.com>
2020-07-24 14:09:36 +0200

    Face Detection with opencv casdace from camera

--- facerecognitionpy/__main__.py
+++ facerecognitionpy/__main__.py
@@ -6,9 +6,17 @@ cap.set(3,640)
 cap.set(4,480)
 cap.set(10,100)
 
+faceCascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
+
 def main():
     while True:
         success, img = cap.read()
+        imGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(imGray,1.1,4)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h),(255,0,0),2)
+            
         cv2.imshow("Video", img)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
