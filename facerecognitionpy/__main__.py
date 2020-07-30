da9625277daa090f15aad0b8d0ae264d327bbcf0
kubaszpak <jakub.szpak.it@gmail.com>
2020-07-30 23:58:36 +0200

    Facing bugs with running program with empty data

--- facerecognitionpy/__main__.py
+++ facerecognitionpy/__main__.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import facerecognitionpy.face_train as face_train
 
 
 cap = cv2.VideoCapture(0)
@@ -9,24 +10,60 @@ cap.set(10,100)
 
 cascade_location = os.path.join(os.getcwd(),'cascades/haarcascade_frontalface_default.xml')
 faceCascade = cv2.CascadeClassifier(cascade_location)
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+delay_value = 200
 
 def main():
-    counter = 0
+    conf_counter = {}
+    label_ids, picture_list = face_train.train()
+    for key in label_ids:
+        conf_counter[key] = 0
+        for key in picture_list:
+            conf_counter[key] += delay_value
+    conf_counter[-1] = 0 # -1 stands for unknown person
     while True:
         success, img = cap.read()
-        imGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(imGray,1.1,5)
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(imgGray,1.1,5)
 
         for (x,y,w,h) in faces:
             cv2.rectangle(img, (x,y), (x+w,y+h),(255,0,0),2)
             roi_color = img[y:y+h , x:x+w]
-            img_location = os.path.join(os.getcwd(),'resources/')
-            my_face = os.path.join(img_location,'my/')
-            os.makedirs(my_face,exist_ok=True)
-            if(counter % 50 == 0):
-                my_face_file = os.path.join(my_face,f'my_pic{counter//50}.png')
-                cv2.imwrite(my_face_file, roi_color)
-            counter += 1
+            roi_gray = imgGray[y:y+h , x:x+w]
+
+            id_, conf = recognizer.predict(roi_gray)
+            if(conf >= 60 and conf < 90):
+                print(label_ids[id_],conf)
+                if(conf_counter[id_] % delay_value == 0):
+                    img_location = os.path.join(os.getcwd(),'resources/')
+                    my_face = os.path.join(img_location,label_ids[id_])
+                    my_face_file = os.path.join(my_face,f'{label_ids[id_]}{conf_counter[id_]//delay_value}.png')
+                    cv2.imwrite(my_face_file, roi_color)
+                conf_counter[id_] += 1
+                conf_counter[-1] = 0
+            else:
+                print('unknown')
+                if(conf_counter[-1] % delay_value == 0):
+                    decision = input('Did a new person just show up? (y/n)')
+                    if decision == 'y':
+                        img_location = os.path.join(os.getcwd(),'resources/')
+                        name = input('What is your name?')
+                        my_face = os.path.join(img_location,name)
+                        try:
+                            os.makedirs(my_face)
+                        except:
+                            print('Try different name this one is probably taken')
+                            my_face = os.path.join(img_location,name)
+                            os.makedirs(my_face)
+                        my_face_file = os.path.join(my_face,f'{name}{conf_counter//delay_value}.png')
+                        cv2.imwrite(my_face_file, roi_color)
+                        label_ids, picture_list = face_train.train()
+                        label_ids[-1] = 0
+                    else:
+                        conf_counter[-1] += 0
+                conf_counter[-1] += 1
         cv2.imshow('Video', img)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
--- facerecognitionpy/face_train.py
+++ facerecognitionpy/face_train.py
@@ -3,25 +3,28 @@ from PIL import Image
 import numpy as np
 import cv2
 
-base_dir = os.getcwd()
-image_dir = os.path.join(base_dir,'resources')
-
-cascade_location = os.path.join(base_dir,'cascades/haarcascade_frontalface_default.xml')
-faceCascade = cv2.CascadeClassifier(cascade_location)
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-# current_id = 0
-labels_ids = {}
-y_train = []
-x_train = []
-
 def train():
+    base_dir = os.getcwd()
+    image_dir = os.path.join(base_dir,'resources')
+
+    cascade_location = os.path.join(base_dir,'cascades/haarcascade_frontalface_default.xml')
+    faceCascade = cv2.CascadeClassifier(cascade_location)
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    current_id = 0
+    label_ids = {}
+    x_train = []
+    y_train = []
+
     for root, dirs, files in os.walk(image_dir):
         for file in files:
             if file.endswith('.png') or file.endswith('.jpg'):
                 path = os.path.join(root,file)
-                label = os.path.basename(root)
-                # print(label, path)
+                label = os.path.basename(root).replace(" ","-").lower()
+                if not label in label_ids.values():
+                    label_ids[current_id] = label
+                    current_id+=1
+
                 pil_image = Image.open(path).convert('L')
                 image_array = np.array(pil_image,'uint8')
                 faces = faceCascade.detectMultiScale(image_array,1.1,5)
@@ -29,7 +32,9 @@ def train():
                 for (x,y,w,h) in faces:
                     roi = image_array[y:y+h,x:x+w]
                     x_train.append(roi)
-                    y_train.append(label)
+                    y_train.append(current_id-1)
 
-    recognizer.train(x_train, y_train)
+    recognizer.train(x_train, np.array(y_train))
     recognizer.save('trainner.yml')
+
+    return label_ids, y_train
