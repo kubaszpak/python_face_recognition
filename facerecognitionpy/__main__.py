47cae11813f59659916c84e60dea18355652a59e
kubaszpak <jakub.szpak.it@gmail.com>
2020-08-19 23:47:17 +0200

    Added poetry

--- facerecognitionpy/__main__.py
+++ facerecognitionpy/__main__.py
@@ -2,9 +2,12 @@ from facerecognitionpy.recognizer import Recognizer
 import cv2
 import eel
 import base64
-# from importlib import reload
+# import sys
+# # from importlib import reload
 
-# reload(recog)
+# # reload(recog)
+
+# print(sys.path)
 
 eel.init('web')
 
@@ -24,11 +27,13 @@ def video_feed():
         blob = blob.decode('utf-8')
         eel.updateImageSrc(blob)()
 
+def main():
+    eel.start('index.html', block=False)
 
-eel.start('index.html', block=False)
+    video_feed()
+        
+    while True:
+        eel.sleep(1.0)
 
-# eel.startTransmision()()
-video_feed()
-
-while True:
-    eel.sleep(1.0)
+if __name__ == "__main__":
+    main()
